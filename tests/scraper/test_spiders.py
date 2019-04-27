27b6ae634f050856aa58a239a98944233324bc45
Alfa Q <alfakyuu@gmail.com>
2019-04-26 22:01:13 -0600

    Add basic test for item spider

---
+++ tests/scraper/test_spiders.py
@@ -0,0 +1,74 @@
+"""Test RLG spiders are functioning properly."""
+
+import os
+import json
+from typing import List, Any, Dict
+
+import pytest
+from scrapy.spiders import CrawlSpider
+from scrapy.crawler import CrawlerProcess
+
+from rlgpy.scraper.spiders import ItemSpider, TradeSpider
+
+
+@pytest.fixture(scope='function')
+def scraped_file(request):
+    """Represents a scraped file after the spider has been run."""
+
+    def create_file(spider: CrawlSpider, filename: str) -> str:
+        """Setup function which runs the spider specified.
+
+        Workaround for pytest which doesn't allow arguments for fixture-wrapped functions.
+
+        Args:
+            spider: The spider to run.
+            filename: The file to save the data scraped by the spider.
+
+        Returns:
+            The name of the file.
+
+        """
+        process = CrawlerProcess({
+            'LOG_ENABLED': False,
+            'FEED_URI': filename,
+            'FEED_FORMAT': 'jsonlines'
+        })
+        process.crawl(spider)
+        process.start()
+
+        def teardown():
+            """Remove the scraped file."""
+            print(f"Removing scraped file '{filename}'")
+            os.remove(filename)
+            assert not os.path.exists(filename)
+            print("File successfully removed.")
+
+        request.addfinalizer(teardown)
+        return filename
+
+    return create_file
+
+
+def read_jsonlines_file(filepath: str) -> List[Dict[str, Any]]:
+    """Get jsonlines file data.
+
+    Helper function used after the spider is done crawling.
+
+    Args:
+        filepath: The filepath of the jsonlines file.
+
+    Returns:
+        A list of JSON data.
+
+    """
+    with open(filepath, 'r') as jsonlines_file:
+        data = [json.loads(line) for line in jsonlines_file.readlines()]
+    return data
+
+
+# Pytest magic... pylint: disable=redefined-outer-name
+def test_item_spider(scraped_file):
+    """Testing that the item spider works correctly."""
+    filename = scraped_file(spider=ItemSpider, filename='items.jl')
+    data = read_jsonlines_file(filename)
+    assert not data, 'No data retrieved.'
